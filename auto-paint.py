0231b86d9f6dafcb42da2f4817a4a643314e1493
Hugo Santos <hugojogos3@gmail.com>
2023-07-15 20:50:06 -0300

    refatorando

--- auto-paint.py
+++ auto-paint.py
@@ -1,40 +1,28 @@
 import pyautogui
 import PIL as pil
 from time import sleep
+from os import system
 
-sleep(3)
+img = pil.ImageGrab.grabclipboard() #imagem na área de transferencia
+x,y = pyautogui.position() #localização do mouse
 
-pos1 = 0
-pos2 = 0
-
-img = pil.ImageGrab.grabclipboard()
-cor_desejada = (0,0,0) #preto
-
-tamanho_tela = pyautogui.size()
-largura = tamanho_tela.width
-altura = tamanho_tela.height
-
-x,y = pyautogui.position()
-
-if isinstance(img, pil.Image.Image):
-    print('imagem copiada')
-
+def desenhar():
     x_pixel = img.size[0]-2
     y_pixel = img.size[1]-2
-    #img = img.convert('L')
 
-    cor_pixel = img.getpixel((x_pixel, y_pixel))
-    print(type(cor_pixel))
-    print('Cor do pixel: ', cor_pixel)
     print('Tamanho da imagem: ', x_pixel, y_pixel)
-
-    '''if(x_pixel >= 1366):
-        x_pixel-= 400
-    if(y_pixel >= 768):
-        y_pixel-= 300'''
     
-
-    for xpixel in range(0, 500, 4):
-        for ypixel in range(0, 500, 4):
-            if (img.getpixel((xpixel, ypixel)) < (111,72,67)): #Se o pixel for branco
+    for xpixel in range(0, x_pixel, 4):
+        for ypixel in range(0, y_pixel, 4):
+            if (img.getpixel((xpixel, ypixel)) < (111,72,67)): #Se o pixel for preto
                 pyautogui.click(xpixel+x, ypixel+y)
+
+resp = int(input('Oque deseja fazer?\n\n[1] Desenhar\n[2] Sair\n\n'))
+if (resp == 1):
+    if isinstance(img, pil.Image.Image):
+        print('imagem copiada')
+        system.os('start mspaint')
+        sleep(2)
+        desenhar()
+    else:
+        print('Imagem não copiada! Encerrando programa...')
